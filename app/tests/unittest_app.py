0ce979cf334ebe00e17c4fb1a3da2642e6a5a16e
Jogil <jogil17@yahoo.es>
2024-12-15 15:41:33 +0100

    cambio en los test2

--- app/tests/unittest_app.py
+++ app/tests/unittest_app.py
@@ -3,8 +3,7 @@ import os
 import unittest
 from unittest.mock import patch, MagicMock
 
-base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
-sys.path.insert(0, base_path)
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 from app.app import app, get_db_connection, get_cache_connection
 
