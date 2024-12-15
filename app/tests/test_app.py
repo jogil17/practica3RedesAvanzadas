6473858af348d7e3c769c6e26a85c51404e0ad76
Jogil <jogil17@yahoo.es>
2024-12-15 13:35:43 +0100

    cambios para pasar fase lint

--- app/tests/test_app.py
+++ app/tests/test_app.py
@@ -1,18 +1,20 @@
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-import warnings
 import pytest
 from app import app
+
+
+base_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, base_path)
+
 
 @pytest.fixture
 def client():
     with app.test_client() as client:
         yield client
 
+
 @pytest.mark.filterwarnings("ignore::DeprecationWarning")
 def test_health_check(client):
     response = client.get("/health")
     assert response.status_code == 200
-
--- app/tests/unittest_app.py
+++ app/tests/unittest_app.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 from app import app, get_db_connection, get_cache_connection
 
+
 class TestApp(unittest.TestCase):
 
     @patch("psycopg2.connect")
@@ -10,57 +11,54 @@ class TestApp(unittest.TestCase):
         mock_connect.return_value = True
         conn = get_db_connection()
         self.assertIsNotNone(conn)
-    
+
     @patch("psycopg2.connect")
     def test_db_connection_failure(self, mock_connect):
         # Simular que la conexión a la base de datos falla
         mock_connect.side_effect = Exception("Error de conexión")
         conn = get_db_connection()
         self.assertIsNone(conn)
-    
+
     @patch("redis.StrictRedis.from_url")
     def test_cache_connection_success(self, mock_redis):
         # Simular que la conexión al caché es exitosa
         mock_redis.return_value.ping.return_value = True
         cache = get_cache_connection()
         self.assertIsNotNone(cache)
-    
+
     @patch("redis.StrictRedis.from_url")
     def test_cache_connection_failure(self, mock_redis):
         # Simular que la conexión al caché falla
-        mock_redis.return_value.ping.side_effect = Exception("Error de conexión")
+        mock_redis.return_value.ping.side_effect = Exception("Error conexión")
         cache = get_cache_connection()
         self.assertIsNone(cache)
 
-    @patch("app.get_db_connection")
-    @patch("app.get_cache_connection")
-    def test_index(self, mock_get_cache, mock_get_db):
-        # Simular respuestas para el endpoint '/'
-        mock_get_db.return_value = True  # Simula que la base de datos está conectada
-        mock_get_cache.return_value = True  # Simula que el caché está conectado
 
-        # Realizar una petición GET al endpoint
-        with app.test_client() as client:
-            response = client.get('/')
-            json_data = response.get_json()
-            
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(json_data["Database"], "Conectado")
-            self.assertEqual(json_data["Cache"], "Conectado")
+@patch("app.get_db_connection")
+@patch("app.get_cache_connection")
+def test_index(self, mock_get_cache, mock_get_db):
+    # Simular respuestas para el endpoint '/'
+    mock_get_db.return_value = True
+    mock_get_cache.return_value = True  # Simula que el caché está conectado
 
-    @patch("app.get_db_connection")
-    @patch("app.get_cache_connection")
-    def test_health_check(self, mock_get_cache, mock_get_db):
-        # Simular que ambos servicios están "conectados"
-        mock_get_db.return_value = True
-        mock_get_cache.return_value = True
-        
-        with app.test_client() as client:
-            response = client.get('/health')
-            json_data = response.get_json()
+    # Realizar una petición GET al endpoint
+    with app.test_client() as client:
+        response = client.get('/')
+        json_data = response.get_json()
 
-            self.assertEqual(response.status_code, 200)
-            self.assertEqual(json_data["status"], "healthy")
+        self.assertEqual(
+            response.status_code,
+            200
+        )
+        self.assertEqual(
+            json_data["Database"],
+            "Conectado"
+        )
+        self.assertEqual(
+            json_data["Cache"],
+            "Conectado"
+        )
+
 
 if __name__ == '__main__':
     unittest.main()
